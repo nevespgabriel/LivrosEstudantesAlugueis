5b295b41ef52ad61bef9f44384cdece446c565c8
nevespgabriel <nevespgabriel@gmail.com>
2024-09-03 09:20:00 -0300

    feat: models terminados

---
+++ src/models/estudante.js
@@ -0,0 +1,23 @@
+let proximoId = 1;
+
+module.exports = (body, id = proximoId) => {
+  if (
+    body.nome != "" &&
+    body.nome != undefined &&
+    body.matricula != "" &&
+    body.matricula != undefined &&
+    body.ano != undefined &&
+    !isNaN(body.ano) &&
+    body.ano > 0 &&
+    body.curso != "" &&
+    body.curso != undefined
+  ) {
+    return {
+      id: id,
+      nome: body.nome,
+      matricula: body.matricula,
+      curso: body.curso,
+      ano: body.ano,
+    };
+  }
+};
---
+++ src/models/livro.js
@@ -0,0 +1,22 @@
+let proximoId = 1;
+
+module.exports = (body, id = proximoId) => {
+  if (
+    body.titulo != "" &&
+    body.titulo != undefined &&
+    body.autor != "" &&
+    body.autor != undefined &&
+    body.ano != undefined &&
+    !isNaN(body.ano) &&
+    body.genero != "" &&
+    body.genero != undefined
+  ) {
+    return {
+      id: id,
+      titulo: body.titulo,
+      autor: body.autor,
+      ano: body.ano,
+      genero: body.genero,
+    };
+  }
+};
